5a856c7a469a27325b5c539164dd64e0ff1fb338
o-oJames <hellocoming1995@gmail.com>
2024-07-12 14:44:15 +0000

    base map added

--- mapviewer/src/App.tsx
+++ mapviewer/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
 import './App.css';
 
-function App() {
+function MapComponent() {
+  useEffect(() => {
+      var osmTileLayer = new TileLayer({
+        source: new OSM(),
+      })
+      var mapview = new View({
+        center: [14960009.811130341, -3001695.7667663265],
+        zoom: 4,
+        minZoom: 0,
+        maxZoom: 25
+      })
+
+      const map = new Map({
+          target: "map",
+          layers: [osmTileLayer],
+          view: mapview
+        });
+    return () => map.setTarget()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="map" className="map-container" />
   );
+}
+
+function App() {
+  return MapComponent();
 }
 
 export default App;
